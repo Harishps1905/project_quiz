4a37713f11cc7aeaaaf857c2b64bea7a504b4425
harish <harishswami62@gmail.com>
2020-04-27 19:21:09 +0530

    location update

--- js/main.js
+++ js/main.js
@@ -16,6 +16,6 @@ function animate() {
     text_2.classList.add("banner-animate-2");
   }, 3000);
   setTimeout(function () {
-    location.replace("/index/index_2.html");
+    window.location.replace("/index/index_2.html");
   }, 5000);
 }
